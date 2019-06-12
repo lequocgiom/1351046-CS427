27e79c0c03adb30fe18947b744b3bd1898566f01
lequocgiom <letrung95@gmail.com>
2019-06-12 22:59:50 +0700

    A03

---
+++ A03-2D Physics and Input System/source/2D Movement/Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;//or right for right landscape 
+
+    }
+
+        void Update () {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		} else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+
+	}
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
+}
